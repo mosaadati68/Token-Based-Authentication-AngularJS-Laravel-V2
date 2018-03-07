cfe25f0714b455dbbbaa55392c84b5c1f23d306a
MoSaadati <mo.saadati68@gmail.com>
2018-03-07 10:57:55 +0330

    First Update

--- public/js/navController.js
+++ public/js/navController.js
@@ -1,6 +1,25 @@
-myApp.controller('navController', ['$scope', 'userModel', function ($scope, userModel) {
+myApp.controller('navController', ['$scope','$location', 'userModel', function ($scope, $location, userModel) {
     angular.extend($scope, {
         user: userModel.getUserObject(),
-        navUrl: []
+        navUrl: [{
+            link: 'Home',
+            url: '/dashboard',
+            submenu: [{
+                link: 'View Gallery',
+                url: '/gallery/view'
+            }, {
+                link: 'Add Gallery',
+                url: '/gallery/add'
+            }, {
+                link: 'Test',
+                url: '/dashboard'
+            }]
+        }]
+    });
+    angular.extend($scope, {
+        doLogout: function () {
+            userModel.doUserLogout();
+            $location.path('/');
+        }
     });
 }]);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,8 @@
 <html ng-app="myApp">
 <head>
     <title>Angular-Laravel Authentication</title>
-    <link rel="stylesheet" href="bower_components\bootstrap\bootstrap.min.css">
+    {{--<link rel="stylesheet" href="bower_components\bootstrap\bootstrap.min.css">--}}
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script> var baseUrl = "{{url('/')}}/";</script>
 </head>
@@ -12,19 +13,23 @@
 </div>
 
 <!-- Application Dependencies -->
-<script src="bower_components/angular/angular.js"></script>
-<script src="bower_components/angular-animate/angular-animate.js"></script>
-<script src="bower_components/angular-route/angular-route.js"></script>
-<script src="bower_components/angular-cookies/angular-cookies.js"></script>
-<script src="bower_components/satellizer/dist/satellizer.js"></script>
+{{--<script type="text/javascript" src="bower_components/jquery/jquery.min.js"></script>--}}
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="bower_components/angular/angular.js"></script>
+<script type="text/javascript" src="bower_components/angular-animate/angular-animate.js"></script>
+<script type="text/javascript" src="bower_components/angular-route/angular-route.js"></script>
+<script type="text/javascript" src="bower_components/angular-cookies/angular-cookies.js"></script>
+<script type="text/javascript" src="bower_components/satellizer/dist/satellizer.js"></script>
 
 <!-- Application Scripts -->
-<script src="js/mainApp.js"></script>
-<script src="js/homeController.js"></script>
-<script src="js/authController.js"></script>
-<script src="js/userController.js"></script>
-<script src="js/navController.js"></script>
-<script src="js/globalController.js"></script>
-<script src="js/userModel.js"></script>
+{{--<script type="text/javascript" src="js/bootstrap.min.js"></script>--}}
+<script type="text/javascript" src="js/mainApp.js"></script>
+<script type="text/javascript" src="js/homeController.js"></script>
+<script type="text/javascript" src="js/authController.js"></script>
+<script type="text/javascript" src="js/userController.js"></script>
+<script type="text/javascript" src="js/navController.js"></script>
+<script type="text/javascript" src="js/globalController.js"></script>
+<script type="text/javascript" src="js/userModel.js"></script>
 </body>
 </html>
